310159504cb849d827dba9b6b761a3d76ec3a60b
chenhaitao <haitaoss@aliyun.com>
2022-08-14 22:14:37 +0800

    Spring 提供的资源加载API

--- spring-mytest/src/main/java/cn/haitaoss/Test.java
+++ spring-mytest/src/main/java/cn/haitaoss/Test.java
@@ -2,8 +2,7 @@ package cn.haitaoss;
 
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Locale;
+import org.springframework.core.io.Resource;
 
 /**
  * @author haitao.chen
@@ -14,8 +13,20 @@ public class Test {
     public static void main(String[] args) throws Exception {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // 或者通过 MessageSourceAware 接口把 MessageSource 注入到bean中
-        System.out.println(context.getMessage("name", null, new Locale("en")));
+        // Spring 提供便利的资源加载方式（本地文件资源、网络资源）
+        Resource resource = context.getResource("/Users/haitao/Desktop/spring-framework/spring-mytest/src/main/java/cn/haitaoss/AppConfig.java");
+        System.out.println(resource.contentLength());
+
+        Resource resource1 = context.getResource("https://www.baidu.com");
+        System.out.println(resource1.contentLength());
+        System.out.println(resource1.getURL());
+
+
+        Resource resource2 = context.getResource("classpath:spring.xml");
+        System.out.println(resource2.contentLength());
+        System.out.println(resource2.getURL());
+        /*// 或者通过 MessageSourceAware 接口把 MessageSource 注入到bean中
+        System.out.println(context.getMessage("name", null, new Locale("en")));*/
 
     }
 }
